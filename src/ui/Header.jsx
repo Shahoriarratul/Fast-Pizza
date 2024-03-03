ff2b629c99b55154c0b687060d5ec642ff4f5ecb
Shahoriar Ratul <36799076+Shahoriarratul@users.noreply.github.com>
2024-03-03 17:59:09 +0600

    Update Header.jsx

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
     return (
         <header className="font-pizza flex items-center justify-between border-b border-stone-500 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
             <Link to="/" className="tracking-widest">
-                Fast React Pizza Co.
+                Fast Pizza Co.
             </Link>
             <SearchOrder />
             <Username />
